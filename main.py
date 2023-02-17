734d720f1c01bef162bc3c98ca5cb328850d209d
aleLovadina <alelova03@gmail.com>
2023-02-17 11:51:13 -0500

    spotify api done

--- main.py
+++ main.py
@@ -5,6 +5,7 @@ import time as t
 
 def main():
     from src.utils import convert_path_to_wav
+    from src.Gwen.APIHandlers.SpotifyAPI.load_cookies import Spotify
     # convert_path_to_wav("C:\\Users\\John\\Documents\\Sound recordings", 'w4a', 'data/Models/SpeechModel/Training/0/')
     # from src.Controllers import AudioController
     
@@ -13,13 +14,13 @@ def main():
     # aud_cntrl.set_mode_sample()
     # aud_cntrl.set_mode_collect_data(10000, os.path.join(os.getcwd(), 'data', 'Models', 'KeywordModel', 'Training', 'Mel_Imgs', '0'), None)
     
-    from src.Gwen.AISystem.Networks import KeywordAudioModel
+    # from src.Gwen.AISystem.Networks import KeywordAudioModel
     
-    model = KeywordAudioModel()
-    model.train(10, model.load_in_data())
+    # model = KeywordAudioModel()
+    # model.train(10, model.load_in_data())
     
     while True:
-        # print('sleeping')
+        print('sleeping')
         t.sleep(1.0)
 
 if __name__ == '__main__':
--- src/Gwen/APIHandlers/SpotifyAPI/load_cookies.py
+++ src/Gwen/APIHandlers/SpotifyAPI/load_cookies.py
@@ -1,50 +1,86 @@
 import pickle
 import spotipy
-import cred
+import os
 from selenium import webdriver
 from selenium.webdriver.chrome.service import Service
 from selenium.webdriver.chrome.options import Options
 from spotipy.oauth2 import SpotifyOAuth
-import time
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
 from selenium.webdriver.common.by import By
 from selenium.common.exceptions import TimeoutException
+import src.utils as utils
+from enum import Enum
 
 class Spotify:
 
+    class StatusCode(Enum):
+        OK = 200
+        CREATED = 201
+        ACCEPTED = 202
+        BAD_REQUEST = 400
+        UNAUTHORIZED = 401
+        FORBIDDEN = 403
+        NOT_FOUND = 404
+        SERVER_ERROR = 500
+        PLAY = 100
+        PAUSE = 101
+        STOP = 102
 
-    #chrome driver
-    webdriver_service = Service(".\chromedriver.exe")
+    def __init__(self):
+        #Import keys from cred.json file
+        self.data_path = os.path.join(os.getcwd(),'data', 'Spotify', 'SpotifyConfig.json')
+        self.config_data = utils.get_json_variables(self.data_path, ['client_id', 'client_secret', 'redirect_uri', 'email', 'password'])
 
-    #options
-    chrome_options=Options()
-    chrome_options.add_argument("--no-sandbox")
-    chrome_options.add_experimental_option("detach", True)
-    #option to remove error: Filed to read descriptor from node connection: A device attached to the system is not functioning
-    chrome_options.add_experimental_option('excludeSwitches', ['enable-logging'])
+        #spotipy API object
+        self._scope = "user-read-playback-state user-modify-playback-state user-read-currently-playing user-read-recently-played streaming user-library-read user-read-private user-library-modify user-read-playback-position playlist-read-private playlist-modify-private"
+        self._sp = spotipy.Spotify(auth_manager=SpotifyOAuth(client_id=self.config_data['client_id'], client_secret=self.config_data['client_secret'], redirect_uri=self.config_data['redirect_uri'], scope=self._scope))
 
-    #create browser
-    browser = webdriver.Chrome(service=webdriver_service, options=chrome_options)
+    def checkCookies(self):
+        try: 
+            pickle.load(open('cookies.pkl', 'rb'))
+            return Spotify.StatusCode.OK.value
+        except:   
+            return Spotify.StatusCode.NOT_FOUND.value
+    
+    def webBrowser(self, value):
+        self.value = value
+        if value==Spotify.StatusCode.OK.value:
+            #chromedriver as environment variable
+            self.webdriver_service = Service(os.getenv('chromedriver'))
+            
+            #chromedriver options
+            self.chrome_options=Options()
+            self.chrome_options.add_argument("--no-sandbox")
+            self.chrome_options.add_experimental_option("detach", True)
+            
+            #option to remove error: Failed to read descriptor from node connection: A device attached to the system is not functioning
+            self.chrome_options.add_experimental_option('excludeSwitches', ['enable-logging'])
 
-    #spotipy object API
-    scope = "user-read-playback-state user-modify-playback-state user-read-currently-playing user-read-recently-played streaming user-library-read user-read-private user-library-modify user-read-playback-position playlist-read-private playlist-modify-private"
-    sp = spotipy.Spotify(auth_manager=SpotifyOAuth(client_id=cred.client_id, client_secret=cred.client_secret, redirect_uri=cred.redirect_uri, scope=scope))
-
-    def __init__(self, user, passw):
-        self.user = user
-        self.passw = passw
-
-    def setup(self):
-
-        try:
-            cookies=pickle.load(open('cookies.pkl', 'rb'))
-            return 'cookies already set'
-        except:
-            pass
+            self.browser = webdriver.Chrome(service=self.webdriver_service, options=self.chrome_options)
+            return Spotify.StatusCode.OK.value
+        
+        #chromedriver as environment variable
+        self.webdriver_service = Service(os.getenv('chromedriver'))
+        
+        #chromedriver options
+        self.chrome_options=Options()
+        self.chrome_options.add_argument("--no-sandbox")
+        self.chrome_options.add_experimental_option("detach", True)
         self.chrome_options.add_argument("--headless")
+        
+        #option to remove error: Filed to read descriptor from node connection: A device attached to the system is not functioning
+        self.chrome_options.add_experimental_option('excludeSwitches', ['enable-logging'])
 
-        #open spotify on a new browser
+        #create browser
+        self.browser = webdriver.Chrome(service=self.webdriver_service, options=self.chrome_options)
+        return Spotify.StatusCode.OK.value
+
+    def createCookies(self):
+        #browser for cookies dump
+        Spotify.webBrowser(self,Spotify.StatusCode.NOT_FOUND.value)
+
+        #open spotify on a new headless browser
         self.browser.get(r"https://accounts.spotify.com/en/login?continue=https%3A%2F%2Fopen.spotify.com%2F")
 
         #wait until page is loaded
@@ -52,16 +88,16 @@ class Spotify:
             WebDriverWait(self.browser, 20).until(EC.presence_of_element_located((By.XPATH, '//*[@id="login-button"]/div[1]')))
             #print('page ready')
         except TimeoutException:
-            print('loading took too much time')
+            return Spotify.StatusCode.SERVER_ERROR.value
 
         #insert email
         email = self.browser.find_element(By.XPATH, '//*[@id="login-username"]')
-        for c in self.user:
+        for c in self.config_data['email']:
             email.send_keys(c)
 
         #insert password
         password = self.browser.find_element(By.XPATH, '//*[@id="login-password"]')
-        for c in self.passw:
+        for c in self.config_data['password']:
             password.send_keys(c)
 
         #click login button
@@ -73,29 +109,23 @@ class Spotify:
             WebDriverWait(self.browser, 20).until(EC.presence_of_element_located((By.XPATH, '//*[@id="main"]/div/div[2]/div[2]/footer/div/div[2]/div/div[1]/button')))
             #print('page ready')
         except TimeoutException:
-            print('loading took too much time')
+            return Spotify.StatusCode.UNAUTHORIZED.value
         
-        #cookies
+        #cookies creation
         cookies=self.browser.get_cookies()
-        #print(cookies)
         pickle.dump(cookies , open("cookies.pkl","wb"))
-
 
         self.browser.quit()
 
-        return 'cookies have been set'
-
-    def open(self):
-        try:
-            self.chrome_options.arguments.remove("--headless")
-        except:
-            pass
+        return Spotify.StatusCode.CREATED  
+        
+    def loadCookies(self):
+        Spotify.webBrowser(self,Spotify.StatusCode.OK.value)
         #open spotify on a new browser
         self.browser.get(r"https://accounts.spotify.com/en/login?continue=https%3A%2F%2Fopen.spotify.com%2F")
 
         #load cookies
         cookies=pickle.load(open('cookies.pkl', 'rb'))
-
         for cookie in cookies:
             cookie['domain']='.spotify.com'
             try:
@@ -103,6 +133,7 @@ class Spotify:
             except:
                 pass
 
+    def openWebPlayer(self):
         #open web player
         self.browser.get('https://open.spotify.com')
         self.browser.maximize_window()
@@ -112,57 +143,67 @@ class Spotify:
             WebDriverWait(self.browser, 20).until(EC.presence_of_element_located((By.XPATH, '//*[@id="main"]/div/div[2]/nav/div[1]/ul/li[1]/a')))
             #print('page ready')
         except TimeoutException:
-            print('loading took too much time')
+            return Spotify.StatusCode.SERVER_ERROR.value
+        
+        #clicking the home button (link) to activate
+        (self.browser.find_element(By.CLASS_NAME, 'eNs6P3JYpf2LScgTDHc6')).click()
 
-        #check for announcement and close it
+        return Spotify.StatusCode.ACCEPTED.value
+    
+    def setup(self):
+        if Spotify.checkCookies(self)==Spotify.StatusCode.OK.value:
+            Spotify.loadCookies(self)
+            Spotify.openWebPlayer(self)
+        elif Spotify.checkCookies(self)==Spotify.StatusCode.NOT_FOUND.value:
+            Spotify.createCookies(self)
+            Spotify.loadCookies(self)
+            Spotify.openWebPlayer(self)
+            #clicking the home button (link) to activate
+            (self.browser.find_element(By.CLASS_NAME, 'eNs6P3JYpf2LScgTDHc6')).click()
+
+    def getDevice(self):
+        #get device id
+        devices=self._sp.devices()
+        device_id=devices['devices'][0]['id']
+        #not needed to transfer playback from one device to another. in case of bugs, try adding it
+        #self._sp.transfer_playback(device_id, force_play=False)
+        return device_id
+    
+    #play track
+    def play(self):
+        self._sp.start_playback()
+        return Spotify.StatusCode.PLAY.value
+
+    #pause track
+    def pause(self):
+        self._sp.pause_playback()
+        return Spotify.StatusCode.PAUSE.value
+    
+    #stop application
+    def stop(self):
         try:
-            WebDriverWait(self.browser, 20).until(EC.presence_of_element_located((By.XPATH, '//*[@id="onetrust-close-btn-container"]/button')))
-            time.sleep(1)
-            xbutton = self.browser.find_element(By.XPATH, '//*[@id="onetrust-close-btn-container"]/button')
-            xbutton.click()
+            self._sp.pause_playback()
         except:
             pass
+        self.browser.quit()
+        return Spotify.StatusCode.STOP.value
 
+    #search query function
     def search(self, query):
+        
         self.query = query
 
-        #get device id
-        devices=self.sp.devices()
-        device_id=devices['devices'][0]['id']
-        self.sp.transfer_playback(device_id, force_play=False)
+        #get id
+        device_id=Spotify.getDevice(self)
 
         # Search for the Song.
-        if(query=='stop'):
-            self.sp.pause_playback()
-            self.browser.quit()
-            return 'Spotify has closed'
-        if(query=='pause'):
-            self.sp.pause_playback()
-            return 'Playback paused'
+        searchQuery = query
 
-        elif(query=='play'):
-            self.sp.start_playback()
-            return 'Playback resumed'
-        else:
-            searchQuery = query
-            #get results
-            trackResults = self.sp.search(searchQuery,1,0,"track")
-            trackURI=trackResults['tracks']['items'][0]['uri']
-            trackSelectedList=[]
-            trackSelectedList.append(trackURI)
-            #sp.transfer_playback(device_id, force_play=True)
-            #print(json.dumps(devices, sort_keys=True, indent=4))
-            self.sp.start_playback(device_id, None, trackSelectedList)
-            print(self.sp.current_user_playing_track())
-            return 
-
-
+        #get results
+        trackResults = self._sp.search(searchQuery,1,0,"track")
+        trackURI=trackResults['tracks']['items'][0]['uri']
+        trackSelectedList=[]
+        trackSelectedList.append(trackURI)
+        self._sp.start_playback(device_id, None, trackSelectedList)
         
-
-ale = Spotify(cred.user, cred.passw)
-
-print(ale.setup())
-
-ale.open()
-q=input("search for song (press stop to close spotify): ")
-ale.search(q)
+        return Spotify.StatusCode.ACCEPTED.value
